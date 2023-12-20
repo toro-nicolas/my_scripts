d50dba6a1d97b5a7b6edf99c80e0e30fe1fe06f7
Nicolas TORO <nicolas.toro@epitech.eu>
2023-12-20 14:30:11 +0100

    Fixing bugs with addsrc.sh

--- addsrc.sh
+++ addsrc.sh
@@ -10,10 +10,10 @@ find src -type f -name '*.c' | while read -r file; do
     
     # Si le fichier n'est pas présent, ajoute une ligne
     if [ $? -ne 0 ]; then
-        sed -i '/^SRCS\t\t+=/a\SRCS\t\t+=\t.c\' Makefile
+        sed -i '/^SRCS\t\t+=\tmain.c/a\SRCS||+=|.c\' Makefile
         echo "You need to add $filename"
     fi
 done
 
 # Remplace les occurrences de "\t" par des vraies tabulations
-sed -i 's/\\t/\t/g' Makefile
+sed -i 's/|/\t/g' Makefile

a2c62cbcc02eee78d5f756d0091cf1d5b8e299d1
Nicolas TORO <nicolas.toro@epitech.eu>
2024-03-20 00:07:21 +0100

    [~] Fix bugs with my_strcmp.c and my_strncmp.c

--- template/lib/my/my_strcmp.c
+++ template/lib/my/my_strcmp.c
@@ -12,6 +12,12 @@ int my_strcmp(char const *s1, char const *s2)
 {
     int i = 0;
 
+    if (s1 == NULL && s2 == NULL)
+        return 0;
+    if (s1 == NULL)
+        return 0 - s2[0];
+    if (s2 == NULL)
+        return s1[0] - 0;
     while (s1[i] != '\0' && s2[i] != '\0') {
         if (s1[i] < s2[i])
             return -1;
--- template/lib/my/my_strncmp.c
+++ template/lib/my/my_strncmp.c
@@ -12,6 +12,12 @@ int my_strncmp(char const *s1, char const *s2, int n)
 {
     int i = 0;
 
+    if (s1 == NULL && s2 == NULL)
+        return 0;
+    if (s1 == NULL)
+        return 0 - s2[0];
+    if (s2 == NULL)
+        return s1[0] - 0;
     while (s1[i] != '\0' && s2[i] != '\0' && i < n) {
         if (s1[i] < s2[i])
             return s1[i] - s2[i];
